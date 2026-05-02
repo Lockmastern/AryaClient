995b85f7d9e1ec49bb3fb93501dda6d45020f97b
Lockmastern <131308920+Lockmastern@users.noreply.github.com>
2026-05-01 19:52:31 -0700

    Add files via upload

---
+++ main/config/config.py
@@ -0,0 +1,10 @@
+# --- Project Config | Arya Client ---
+
+title = 'AryaClient'
+main_version = '0.0.1'
+updater_version = '0.0.1'
+license = 'General Purpose'
+author = 'kinichii'
+github_repo = '' # Need to set URL
+update_repo = '' # almost same path as github repo except not
+config_repo = '' # once again
